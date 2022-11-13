67d794d16c5cae2079e97eba922429b492c729c0
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-11-13 13:00:13 +0530

    Add files via upload

---
+++ adminpage.aspx.vb
@@ -0,0 +1,95 @@
+﻿
+Imports System.Data.OleDb
+Imports System.Data
+
+Imports System.Data.DataView
+Imports System.Data.DataRowView
+Public Class adminpage
+    Inherits System.Web.UI.Page
+
+    Dim cn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\HP\source\repos\contactBook\My Project\contactBook.mdb")
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim str = "select * from [user]"
+        Dim cmd As New OleDbCommand(str, cn)
+        Dim adp = New OleDbDataAdapter(cmd)
+        Dim ds As New DataSet
+        adp.Fill(ds)
+        'GridViewUser.DataSource = ds.Tables(0)
+        'GridViewUser.DataBind()
+
+    End Sub
+
+    Protected Sub btndel_Click(sender As Object, e As EventArgs) Handles btndel.Click
+        Try
+            Dim qry As String
+            Dim res As Integer
+
+            qry = "delete from [user] where username='" & DropDownUser.SelectedValue & "'and password='" & tbpass.Text & "'"
+
+            Dim cmd As New OleDbCommand(qry, cn)
+            cn.Open()
+            cmd.ExecuteNonQuery()
+            GridViewUser.DataBind()
+            MsgBox("Deleted")
+            cn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnupd_Click(sender As Object, e As EventArgs) Handles btnupd.Click
+        Try
+            Dim qry As String
+
+            qry = "update [user] set username='" & DropDownUser.SelectedValue & "'  and password='" & tbpass.Text & "'"
+
+            cn.Open()
+            Dim cmd = New OleDbCommand(qry, cn)
+            cmd.ExecuteNonQuery()
+            GridViewUser.DataBind()
+            cn.Close()
+            MsgBox("Updated")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btndel0_Click(sender As Object, e As EventArgs) Handles btndel0.Click
+        Try
+            Dim qry As String
+            Dim res As Integer
+
+
+            qry = "delete from contact where cnum=" & tbnum.Text & ""
+
+            Dim cmd As New OleDbCommand(qry, cn)
+            cn.Open()
+            res = cmd.ExecuteNonQuery
+            GridViewUser.DataBind()
+            cn.Close()
+            MsgBox("Deleted")
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnupd0_Click(sender As Object, e As EventArgs) Handles btnupd0.Click
+        Try
+            Dim qry As String
+            Dim res As Integer
+
+            qry = "update [contact] set username='" & tbnum.Text & "'and password='" & tbpass.Text & "'"
+
+            Dim cmd As New OleDbCommand(qry, cn)
+            cn.Open()
+            res = cmd.ExecuteNonQuery
+            GridViewUser.DataBind()
+            cn.Close()
+            MsgBox("Updated")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
---
+++ userpage.aspx.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Data.OleDb
+
+Public Class mainHome
+    Inherits System.Web.UI.Page
+
+    Dim cn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\HP\source\repos\contactBook\My Project\contactBook.mdb")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Try
+            Dim qry As String
+            Dim field1 As String = CType(Session.Item("user"), String)
+
+            qry = "insert into contacts(cname,cnum,ccity,created_by) values('" & tbname.Text & "'," & tbnum.Text & ",'" & tbcity.Text & "','" & field1 & "')"
+            cn.Open()
+            Dim cmd As New OleDbCommand(qry, cn)
+            Dim res As Integer
+            cmd.ExecuteNonQuery()
+            GridView1.DataBind()
+
+            cn.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnUpd_Click(sender As Object, e As EventArgs) Handles btnUpd.Click
+        Try
+            Dim qry As String
+
+            qry = "update [contact] set cname='" & tbname.Text & "'  and cnum='" & tbnum.Text & "' and ccity='" & tbcity.Text & "'"
+
+            cn.Open()
+            Dim cmd = New OleDbCommand(qry, cn)
+            cmd.ExecuteNonQuery()
+            'GridViewUser.DataBind()
+            cn.Close()
+            MsgBox("Updated")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnDel_Click(sender As Object, e As EventArgs) Handles btnDel.Click
+        Try
+            Dim qry As String
+
+            qry = "delete from [contact] where cname='" & tbname.Text & "'"
+
+            cn.Open()
+            Dim cmd = New OleDbCommand(qry, cn)
+            cmd.ExecuteNonQuery()
+            'GridViewUser.DataBind()
+            cn.Close()
+            MsgBox("Updated")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Protected Sub btnSrcName_Click(sender As Object, e As EventArgs) Handles btnSrcName.Click
+        Try
+            cn.Open()
+            Dim qry As String
+            qry = "select * from [contact] where cname"
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
